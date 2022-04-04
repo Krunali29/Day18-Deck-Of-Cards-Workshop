9760a98cccfff3ea5614ab3d7e8e748db3a8f1b2
Krunali <krunalilole29@gmail.com>
2022-04-05 01:30:51 +0530

    Adding UC 4 Deck of Cards To Shuffle the Cards

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -6,7 +6,7 @@ public class DeckOfCards extends Players
     private String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
     int n = 4 * 13;
 
-    void deckInitialize()
+    public String[] deckInitialize()
     {
         String[] deck = new String[this.n];
         for (int i = 0; i < 13; i++)
@@ -23,6 +23,18 @@ public class DeckOfCards extends Players
             deck[r] = deck[i];
             deck[i] = temp;
         }
+        return deck;
+    }
+    public String[] shuffleCard(String[] deck)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int r = i + (int) (Math.random() * (n - i));
+            String temp = deck[r];
+            deck[r] = deck[i];
+            deck[i] = temp;
+        }
+        return deck;
     }
     public static void main(String[] args)
     {
@@ -30,5 +42,7 @@ public class DeckOfCards extends Players
         deckOfCards.deckInitialize();
         deckOfCards.addingPlayer();
         deckOfCards.playerSequence();
+        String[] deck = deckOfCards.deckInitialize();
+        String[] shuffledDeck = deckOfCards.shuffleCard(deck);
     }
 }
