648291e5c582d4feb677a1ced4ca178545af655e
Krunali <krunalilole29@gmail.com>
2022-04-05 01:40:52 +0530

    Adding UC 5 Deck of Cards To distribute in order to players

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -16,19 +16,12 @@ public class DeckOfCards extends Players
                 deck[4 * i + j] = ranks[i] + " of " + suits[j];
             }
         }
-        for (int i = 0; i < n; i++)
-        {
-            int r = i + (int) (Math.random() * (n - i));
-            String temp = deck[r];
-            deck[r] = deck[i];
-            deck[i] = temp;
-        }
+        deck = shuffleCard(deck);
         return deck;
     }
     public String[] shuffleCard(String[] deck)
     {
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             int r = i + (int) (Math.random() * (n - i));
             String temp = deck[r];
             deck[r] = deck[i];
@@ -36,13 +29,45 @@ public class DeckOfCards extends Players
         }
         return deck;
     }
-    public static void main(String[] args)
-    {
+    int[] nineCardsIndex() {
+        int[] cardIndex = { 52, 52, 52, 52, 52, 52, 52, 52, 52 }; //Initialize array with numbers
+        int i = 0;
+        int min = 0;
+        int max = 51;
+        while (i < 9) {
+            int condition = 0;
+            int randomIndex = (int) Math.floor(Math.random() * (max - min) + min);
+            for (int j = 0; j < cardIndex.length; j++) {
+                if (randomIndex == cardIndex[j]) {
+                    condition = 1;
+                    break;
+                }
+            }
+            if (condition == 0) {
+                cardIndex[i] = randomIndex;
+                i += 1;
+            }
+        }
+        return cardIndex;
+    }
+    public void distribution() {
+        String[] player = playerSequence();
+        String[] deck = deckInitialize();
+        int[] cardIndex = nineCardsIndex();
+        System.out.println(cardIndex);
+        for (int i = 1; i < player.length + 1; i++) {
+            System.out.println("Player " + player[i - 1] + " Cards :");
+            for (int j = i - 1; j < cardIndex.length; j = j + player.length) {
+                System.out.printf("|| %s    ", deck[j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
         DeckOfCards deckOfCards = new DeckOfCards();
-        deckOfCards.deckInitialize();
-        deckOfCards.addingPlayer();
-        deckOfCards.playerSequence();
-        String[] deck = deckOfCards.deckInitialize();
-        String[] shuffledDeck = deckOfCards.shuffleCard(deck);
+        deckOfCards.distribution();
+        // String[] deck = deckOfCards.deckInitialize();
+
     }
 }
